b0e9fad31bb3da6785ebd77e6079dcc609f9ba1a
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-21 09:52:36 -0600

    import packages

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import 'dotenv/config'
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
+import { check, validationResult } from "express-validator";
+
